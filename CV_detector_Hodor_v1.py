7f29fbef93bf4a604e32a665aefed08837beb204
Alejo Valenzuela <alejo.valenzuela.electronica@gmail.com>
2024-04-12 01:37:08 -0300

    Update .py and README

--- CV_detector_Hodor_v1.py
+++ CV_detector_Hodor_v1.py
@@ -1,7 +1,3 @@
-# Proyecto de automatizacion con vision artificial
-# Integrantes:
-#   - Gutierrez Emiliano
-#   - Valenzuela Alejo
 
 import cv2
 from ultralytics import YOLO
@@ -9,26 +5,35 @@ import random
 import math
 import os
 
-def distance(x1,y1,x2,y2):
-    # x1, y1 son los puntos del centro inferior de la imagen
-    # x2, y2 son los puntos del centro inferior del recuadro del obstaculo (punto que se dibuja)
+import numpy as np
+from scipy.interpolate import interp1d
 
-    #Calculo el modulo entre puntos, y lo asumo como la altura
-    dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+######################################
 
-    #Calculo el beta con el modulo
-    beta = -35 / 360 * (360-dist) + 35
+# Puntos dados (px, distance_cm)
+puntos = np.array([(343,324.5), (370,264.5), (413,204.5), (493,144.5), (570,114.5), (690,84.5), (720,77.3)])
 
-    #Con beta calculo la distancia aproximada
-    return -25.71 * (beta - 90) / beta
+# Separamos las coordenadas x e y de los puntos
+x = puntos[:, 0]
+y = puntos[:, 1]
+
+# Interpolamos una función para aproximar distancias
+f_interp = interp1d(x, y, kind='cubic')
+
+# Creamos los puntos para la interpolación
+x_interp = np.linspace(min(x), max(x), 100)
+y_interp = f_interp(x_interp)
+
+########################################
 
 # Modelo e imagenes a color
-#model = YOLO('trainedYOLO_color.pt')
-#source = '/dataset_frames'
+model = YOLO('../yolov8n_train_20-3-24.pt')
+source = '../dataset_frames'
+#source = '../bin_dataset_2'
 
 # Modelo e imagenes a escala de grises
-model = YOLO('trainedYOLO_gray.pt')
-source = '/gray_dataset_frames'
+# model = YOLO('../best_grayDataset.pt')
+# source = '../gray_dataset_frames'
 
 
 # Obtener la lista de nombres de archivos de la carpeta
@@ -51,10 +56,12 @@ image_width = 1280
 image_height = 720
 
 apAng = 35  #grados
+altura_camara = 27.6
+distancia_inferior = 24.5+60
 
 while True:
     #ret, frame = cap.read()
-    # Itero sobre los nombres de archivos y leer las imágenes
+    # Iterar sobre los nombres de archivos y leer las imágenes
     for nombre_archivo in nombres_archivos:
         ruta_imagen = os.path.join(source, nombre_archivo)
         frame = cv2.imread(ruta_imagen)
@@ -66,6 +73,7 @@ while True:
         for result in results:
             for r in result.boxes.data.tolist():
                 print(r)
+                #x1, y1, x2, y2, id, score, class_id = r
                 try:
                     x1, y1, x2, y2, id, score, class_id = r
                 except:
@@ -75,53 +83,56 @@ while True:
                 y1 = int(y1)
                 y2 = int(y2)
                 id = int(id)
+                if y2 > 343 and y2 <= 720:
+                    print(f'y1: {y1}, y2: {y2}')
+                     # Calcular las coordenadas del punto en el borde inferior del centro del objeto
+                    center_x = (x1 + x2) / 2
+                    bottom_center_y = y2
+                    # Convertir las coordenadas a píxeles en una imagen de 1280 x 720
+                    #pixel_x = int(center_x * image_width)
+                    #pixel_y = int(bottom_center_y * image_height)
+                    # Dibujar un punto en el centro del borde inferior del objeto
+                    #cv2.circle(frame, (pixel_x, pixel_y), radius=5, color=(0, 0, 255), thickness=-1)
 
-                # Calculo las coordenadas del punto en el borde inferior del centro del objeto
-                center_x = (x1 + x2) / 2
-                bottom_center_y = y2
+                    w, h = (x2 - x1), (y2 - y1)         #Calculo las coordenadas del punto
+                    #cx, cy = x1 + w // 2, y1 + h // 2
+                    cx, cy = x1 + w // 2, y2
+                    #distancia = distance(cx, 0, cx, cy)
+                    distancia = f_interp(y2)
 
-                # Calculo las coordenadas del punto
-                w, h = (x2 - x1), (y2 - y1)        
-                cx, cy = x1 + w // 2, y2
+                    class_id = int(class_id)
+                    #Tomo el nombre de la clase
+                    class_name=classNames[int(class_id)]
+                    #Calculo el nivel de la prediccion
+                    conf=math.ceil((score*100))/100
+                    #Armo un texto para mostrar sobre el recuadro
+                    #label=f'{class_name},{conf}'
+                    label = f'{class_name}, conf: {conf}'
 
-                # Calculo la distancia
-                distancia = distance(cx, 0, cx, cy)
+                    #Dibujo el recuadro con un color diferente para cada deteccion
+                    cv2.rectangle(frame, (x1,y1), (x2,y2), (colors[id % len(colors)]), 3)
 
-                class_id = int(class_id)
-                #Tomo el nombre de la clase
-                class_name=classNames[int(class_id)]
-                #Calculo el nivel de la prediccion
-                conf=math.ceil((score*100))/100
+                    #Calculo el largo del texto a mostrar
+                    t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                    c2=x1+t_size[0]-90,y1 - t_size[1]
 
-                # Muestro los datos
-                #Armo un texto para mostrar sobre el recuadro
-                #label=f'{class_name},{conf}'
-                label = f'{class_name}, conf: {conf}'
+                    cv2.circle(frame, (cx, cy), 5, (0, 0, 0), cv2.FILLED)
 
-                #Dibujo el recuadro con un color diferente para cada deteccion
-                cv2.rectangle(frame, (x1,y1), (x2,y2), (colors[id % len(colors)]), 3)
+                    #Dibujo el rectangulo ajustado al largo del texto
+                    cv2.rectangle(frame,(x1,y1),c2,(colors[id]),-1,cv2.LINE_AA)
 
-                #Calculo el largo del texto a mostrar
-                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                c2=x1+t_size[0]-90,y1 - t_size[1]
+                    start_point = (640, 720)
+                    end_point = (cx, cy)
+                    thickness = 2
+                    color = (colors[id % len(colors)])
 
-                cv2.circle(frame, (cx, cy), 5, (0, 0, 0), cv2.FILLED)
+                    # Dibujo una línea desde el punto de inicio al punto final
+                    cv2.arrowedLine(frame, start_point, end_point, (colors[id % len(colors)]), thickness)
+                    label_dist = f'distancia: {distancia:.2f} cm'
 
-                #Dibujo el rectangulo ajustado al largo del texto
-                cv2.rectangle(frame,(x1,y1),c2,(colors[id]),-1,cv2.LINE_AA)
-
-                start_point = (640, 720)
-                end_point = (cx, cy)
-                thickness = 2
-                color = (colors[id % len(colors)])
-
-                # Dibujo una línea desde el punto de inicio al punto final
-                cv2.arrowedLine(frame, start_point, end_point, (colors[id % len(colors)]), thickness)
-                label_dist = f'distancia: {distancia:.2f}'
-
-                #Coloco el texto
-                cv2.putText(frame, label, (x1, y1-2), 0, 0.5, (255,255,255), thickness=1, lineType=cv2.LINE_AA)
-                cv2.putText(frame, label_dist, (cx, cy), 0, 0.5, (255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
+                    #Coloco el texto
+                    cv2.putText(frame, label, (x1, y1-2), 0, 0.5, (255,255,255), thickness=1, lineType=cv2.LINE_AA)
+                    cv2.putText(frame, label_dist, (cx, cy-4), 0, 0.5, (255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
 
         cv2.imshow("frame", frame)
         cv2.imwrite('./results/'+nombre_archivo, frame)
--- README.md
+++ README.md
@@ -36,10 +36,16 @@ El trabajo desarrollado se dividio en:
 
 - Se asume que el horizonte se encuentra en el infinito, de manera que todos los obstaculos estaran siempre en la parte inferior de la imagen.
 
-- Se asume que la altura de la camara sera siempre fija a 35 cm.
+- Se asume que la altura de la camara sera siempre fija a `27.6 cm`.
 
 ## Corner Cases
 
-- En caso de tener un obstaculo que se choque con el borde inferior de la imagen (py = 0), entonces se asume que esta en una distancia entre o y `xx`
+- En caso de tener un obstaculo que se choque con el borde inferior de la imagen (py = 0), entonces se asume que esta en una distancia entre `0 cm` y `84.3 cm`
 
 - Si bien se comunicaran todos los obstaculos detectados, solamente nos interesa comunicar con detalle el obstaculo mas cercano, pero en caso de que hayan varios a la misma distancia, dejaremos que el grupo de movimiento sea el encargado de decidir que hacer con el movimiento del robot.
+
+## Funcionamiento
+
+En este caso, el sistema se encarga de detectar las clases de objetos del mundo de obstaculos definidos, y luego se procesa la informacion referida a la distancia y angulo a la que se encuentra dicho obstaculo para ser comunicado a la etapa de movimiento.
+
+<a href="https://github.com/AlejoValenzuela73/HodorComputerVision"> <img src="./HODOR_detection.gif"> </a>
